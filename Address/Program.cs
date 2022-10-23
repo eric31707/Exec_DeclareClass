7de5a318a32d2b03ad4eb15bb0cb23bccd358765
eric31707 <eric31707@DESKTOP-95TUUP6>
2022-10-23 21:13:16 +0800

    加入專案檔案。

---
+++ Address/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	class Address
+	{								
+		public string City { get; set; }
+		public string ZipCode { get; set; }
+		public string Street { get; set; }
+	}
+}
---
+++ Telephone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephone
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	class Telephone
+	{
+		public string CountryCode { get; set; }
+		public string AreaCode { get; set; }
+		public string Number { get; set; }
+		public string Ext { get; set; }
+	}
+}
